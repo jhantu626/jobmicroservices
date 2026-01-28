87d059e9a7755fce24fca9131842c2eea1cad6b8
jhantu626 <jhantubala626@gmail.com>
2026-01-28 16:50:24 +0530

    config server added

---
+++ JobService/src/main/java/io/app/client/CompanyClient.java
@@ -0,0 +1,15 @@
+package io.app.client;
+
+import io.app.dto.ApiResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "CompanyService")
+public interface CompanyClient {
+
+    @GetMapping("/company/check/{id}")
+    public ApiResponse companyExist(@PathVariable("id") long id);
+}
--- JobService/src/main/java/io/app/service/impl/JobServiceImpl.java
+++ JobService/src/main/java/io/app/service/impl/JobServiceImpl.java
@@ -1,9 +1,13 @@
 package io.app.service.impl;
 
+import io.app.client.CompanyClient;
 import io.app.dto.ApiResponse;
+import io.app.exception.RequiredFieldException;
+import io.app.exception.ResourceNotFoundException;
 import io.app.model.Job;
 import io.app.repository.JobRepository;
 import io.app.service.JobService;
+import org.hibernate.ResourceClosedException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -13,14 +17,23 @@ import org.springframework.stereotype.Service;
 public class JobServiceImpl implements JobService {
 
     private final JobRepository repository;
+    private final CompanyClient companyClient;
 
-    public JobServiceImpl(JobRepository repository){
+    public JobServiceImpl(JobRepository repository,CompanyClient companyClient){
         this.repository=repository;
+        this.companyClient=companyClient;
     }
 
 
     @Override
     public ApiResponse createJob(Job job) {
+        if (job.getCompanyId()==0){
+            throw new RequiredFieldException("Please Provide Company");
+        }
+        ApiResponse apiResponse=companyClient.companyExist(job.getCompanyId());
+        if (!apiResponse.status()){
+            throw new ResourceNotFoundException("Company Does not exist");
+        }
         repository.save(job);
         return new ApiResponse("Job Created Successfully",true);
     }
