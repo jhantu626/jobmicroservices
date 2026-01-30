6c0fe1838718d1cb8689a4308b2853316c154c53
jhantu626 <jhantubala626@gmail.com>
2026-01-30 13:41:35 +0530

    created the api for create job successfully

--- ReviewService/src/main/java/io/app/model/Review.java
+++ ReviewService/src/main/java/io/app/model/Review.java
@@ -3,9 +3,7 @@ package io.app.model;
 import io.app.exception.RequiredFieldException;
 import jakarta.persistence.*;
 import lombok.*;
-import org.aspectj.apache.bcel.classfile.Module;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -17,15 +15,17 @@ import java.time.LocalDateTime;
 public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private double rating;
     private String description;
     @Enumerated(EnumType.STRING)
     private ReviewType reviewType;
-    private long jobId;
-    private long companyId;
-    private long likeCount;
-    private long dislikeCount;
+    @Column(nullable = true)
+    private Long jobId;
+    @Column(nullable = true)
+    private Long companyId;
+    private Long likeCount;
+    private Long dislikeCount;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
@@ -35,15 +35,15 @@ public class Review {
             throw new RequiredFieldException("Description Required");
         }else if (rating<0 || rating>5){
             throw new RequiredFieldException("Rating should between 0 to 5");
-        }else if (reviewType==ReviewType.REVIEW && companyId==0){
+        }else if (reviewType==ReviewType.REVIEW && companyId==null){
             throw new RequiredFieldException("Company Id is required");
-        }else if (reviewType==ReviewType.JOB && companyId==0){
+        }else if (reviewType==ReviewType.JOB && companyId==null){
             throw new RequiredFieldException("Job Id is required");
         }
 
 
-        this.likeCount=0;
-        this.dislikeCount=0;
+        this.likeCount=new Long(0);
+        this.dislikeCount=new Long(0);
         this.createdAt=LocalDateTime.now();
         this.updatedAt= LocalDateTime.now();
     }
