18b3a0fc39ac397b4d2e5a503bb5ac3cecfd3b3b
jhantu626 <jhantubala626@gmail.com>
2026-01-30 13:47:53 +0530

    refactored the long to int

--- ReviewService/src/main/java/io/app/service/ReviewService.java
+++ ReviewService/src/main/java/io/app/service/ReviewService.java
@@ -6,6 +6,6 @@ import org.springframework.data.domain.Page;
 
 public interface ReviewService {
     public ApiResponse createReview(Review review);
-    public Page<Review> getReviewsByCompanyId(long companyId,long pageNo,long size);
-    public Page<Review> getReviewsByJobId(long jobId,long pageNo,long size);
+    public Page<Review> getReviewsByCompanyId(long companyId,int pageNo,int size);
+    public Page<Review> getReviewsByJobId(long jobId,int pageNo,int size);
 }
